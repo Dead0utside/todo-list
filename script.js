82d87d5f3328adcbe583f56c5fe8424f43b91217
Volodymyr Hutsuliak <volodya1234321@gmail.com>
2024-10-18 15:05:51 +0200

    resolve some naming conflicts

--- script.js
+++ script.js
@@ -7,46 +7,57 @@ class Task {
 	}
 }
 
-class ToDoList {
-	constructor(id, name) {
-		this.id = id;
-		this.name = name;
-		this.tasks = [];
-	}
-	color;
-}
+// class ToDoList {
+// 	constructor(id, name) {
+// 		this.id = id;
+// 		this.name = name;
+// 		this.tasks = [];
+// 	}
+// 	color;
+// }
 
 const todoList1 = document.getElementById("todolist-1");
 const taskInputForm1 = document.getElementById("input-1");
 const addTaskButton1 = document.getElementById("submit-1");
 const taskList1 = document.getElementById("task-list-1");
 
-addTaskButton1.addEventListener("click", () => addTask());
+let tasks = [];
+let taskId = 0;
 
-function addTask() {
+addTaskButton1.addEventListener("click", () =>
+	addTask(tasks, taskId, taskInputForm1)
+);
+
+function addTask(tasks, taskId, taskInputForm) {
+	const taskName = taskInputForm.value;
+
+	let task = new Task(taskId);
+
 	const newLi = document.createElement("li");
 
 	let newTaskElement = document.createElement("section");
 	newTaskElement.className = "task-container";
 
-	const checkbox = document.createElement("input");
-	checkbox.className = "checkbox";
-	checkbox.type = "checkbox";
+	const checkboxElement = document.createElement("input");
+	checkboxElement.className = "checkbox";
+	checkboxElement.type = "checkbox";
 
-	const taskName = document.createElement("p");
-	taskName.className = "task-name";
-	taskName.textContent = taskInputForm1.value;
+	const taskNameElement = document.createElement("p");
+	taskNameElement.className = "task-name";
+	taskNameElement.textContent = taskName;
 	taskInputForm1.value = "";
 
 	const deleteBtn = document.createElement("button");
 	deleteBtn.className = "delete-btn";
 	deleteBtn.id = "delete-btn";
 
-	newTaskElement.appendChild(checkbox);
-	newTaskElement.appendChild(taskName);
+	newTaskElement.appendChild(checkboxElement);
+	newTaskElement.appendChild(taskNameElement);
 	newTaskElement.appendChild(deleteBtn);
 
 	newLi.appendChild(newTaskElement);
 
 	taskList1.appendChild(newLi);
+
+	tasks.push();
 }
