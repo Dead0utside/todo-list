f9f8077db36b5e6b99df52f9e08976bc711f21fd
Volodymyr Hutsuliak <volodya1234321@gmail.com>
2024-10-18 12:42:28 +0200

    start working on list addition

--- script.js
+++ script.js
@@ -14,27 +14,57 @@ class ToDoList {
 		this.tasks = [];
 	}
 	color;
+
+	addTask(task) {
+		let newTaskElement = document.createElement("section");
+		newTaskElement.className = "task-container";
+
+		const taskName = document.createElement("p");
+		taskName.className = "task-name"; // Set the class
+		taskName.textContent = taskInputForm1.value;
+
+		const checkbox = document.createElement("input");
+		checkbox.type = "checkbox";
+
+		newTaskElement.appendChild(taskName);
+		newTaskElement.appendChild(checkbox);
+
+		todoList1.appendChild(newTaskElement);
+	}
 }
 
-const todoList1 = document.getElementById("todolist-1");
-const taskInputForm1 = document.getElementById("input-1");
-const addTaskButton1 = document.getElementById("submit-1");
+todoLists = [];
+workspace = document.querySelector(".workspace");
 
-addTaskButton1.addEventListener("click", () => addTask());
+function addToDoList(id, name) {
+	const toDoList = document.createElement("article");
+	toDoList.className = "todo-list";
+	toDoList.id = `todolist-${id}`;
 
-function addTask() {
-	let newTaskElement = document.createElement("section");
-	newTaskElement.className = "task-container";
+	const taskInput = document.createElement("input");
+	taskInput.id = `input-${id}`;
+	taskInput.className = "task-input";
+	taskInput.type = "text";
+	taskInput.placeholder = "New task";
 
-	const taskName = document.createElement("p");
-	taskName.className = "task-name"; // Set the class
-	taskName.textContent = taskInputForm1.value;
+	const submitInput = document.createElement("input");
+	submitInput.id = `submit-${id}`;
+	submitInput.className = "task-submit-btn";
+	submitInput.type = "submit";
+	submitInput.value = "+";
 
-	const checkbox = document.createElement("input");
-	checkbox.type = "checkbox";
+	const taskList = document.createElement("ul");
+	taskList.class = "task-list";
 
-	newTaskElement.appendChild(taskName);
-	newTaskElement.appendChild(checkbox);
+	toDoList.appendChild(taskInput);
+	toDoList.appendChild(submitInput);
+	toDoList.appendChild(taskList);
 
-	todoList1.appendChild(newTaskElement);
+	document.workspace.appendChild(toDoList);
 }
+
+// const todoList1 = document.getElementById("todolist-1");
+// const taskInputForm1 = document.getElementById("input-1");
+// const addTaskButton1 = document.getElementById("submit-1");
+
+// addTaskButton1.addEventListener("click", () => addTask());
